9380b13f2130fa6354e07e0a2c6a97d09fceb035
usagiga <usagiga@usagiga.me>
2023-10-23 09:06:53 +0900

    chore: add Playwright

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from '@playwright/test';
+
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
